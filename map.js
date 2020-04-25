92b2d9a92bbf7323bcf9448198fd8e15fc51f894
Virus936 <lok.vira@gmail.com>
2020-04-25 23:09:28 +0200

    reorganisation du fichier (fonction au debut du fichier)

--- map.js
+++ map.js
@@ -1,29 +1,3 @@
-var mymap = L.map('mapid').setView([46.89040 , 2.79290],15 );
-var bounds = new L.LatLngBounds([[40.31583 , -5.43097], [51.59686 , 9.23169]]);
-mymap.fitBounds(bounds);
-
-
-L.tileLayer('', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,',
-    minZoom: 6,
-    maxZoom: 10,
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: ''
-}).addTo(mymap);
-
-geojson = L.geoJson(data, {
-    onEachFeature : onEachFeature,
-    style : {
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 1,
-        fillColor:'lightblue',
-    },
-}).addTo(mymap);
-
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
@@ -58,38 +32,6 @@ function resetHighlight(e) {
 }
 
 
-
-var info = L.control();
-
-info.onAdd = function (mymap) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-        '<b>' + props.nom + '</b><br />' + datacovid[ props.code ]['hosp']    + ' people / mi<sup>2</sup>'
-        : 'Hover over a state');
-};
-
-info.addTo(mymap);
-datacovid={}
-donnees_link = 'https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7'
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            datacovid =  data4map(xhttp.response.replace(/"/g,''))
-        }
-    };
-    xhttp.open("GET", donnees_link, true);
-    xhttp.send();
-}
-loadDoc()
-
-
 //var csv is the CSV file with headers
 function data4map(csv){
 
@@ -118,3 +60,65 @@ function data4map(csv){
   //return result; //JavaScript object
   return result; //JSON
 }
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            datacovid =  data4map(xhttp.response.replace(/"/g,''))
+        }
+    };
+    xhttp.open("GET", donnees_link, true);
+    xhttp.send();
+}
+
+var mymap = L.map('mapid').setView([46.89040 , 2.79290],15 );
+var bounds = new L.LatLngBounds([[40.31583 , -5.43097], [51.59686 , 9.23169]]);
+mymap.fitBounds(bounds);
+
+
+L.tileLayer('', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,',
+    minZoom: 6,
+    maxZoom: 10,
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: ''
+}).addTo(mymap);
+
+geojson = L.geoJson(data, {
+    onEachFeature : onEachFeature,
+    style : {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 1,
+        fillColor:'lightblue',
+    },
+}).addTo(mymap);
+
+
+
+
+var info = L.control();
+
+info.onAdd = function (mymap) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.nom + '</b><br />' + datacovid[ props.code ]['hosp']    + ' people / mi<sup>2</sup>'
+        : 'Hover over a state');
+};
+
+info.addTo(mymap);
+datacovid={}
+donnees_link = 'https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7'
+loadDoc()
+
+
